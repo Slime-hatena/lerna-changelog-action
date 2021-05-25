1f6e5886933ac8ca87317882c2cffcb176d26ea5
Slime-hatena <git@aki-memo.net>
2021-05-26 03:27:50 +0900

    test: add a test for Output.ts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,9 +1,9 @@
-import {info} from '../src/Output';
+import {info, br, success, warn, error} from '../src/Output';
 import {Changelog} from '../src/Changelog';
 import * as process from 'process';
 import * as fs from 'fs';
 
-test('create markdown', async () => {
+test('check Changelog.ts', async () => {
     process.env['GITHUB_AUTH'] = fs.readFileSync('./.GITHUB_AUTH').toString();
 
     const from = '4b825dc642cb6eb9a060e54bf8d69288fbee4904';
@@ -22,4 +22,53 @@ test('create markdown', async () => {
 
     const markdown = await changelog.generate(from, to);
     info(markdown);
+});
+
+test('check Output.ts', async () => {
+    const toEqualData = [
+        {
+            value: br(),
+            result: undefined
+        },
+        {
+            value: br(true),
+            result: ''
+        },
+        {
+            value: info('info message'),
+            result: undefined
+        },
+        {
+            value: info('info message', true),
+            result: 'info message'
+        },
+        {
+            value: success('success message'),
+            result: undefined
+        },
+        {
+            value: success('success message', true),
+            result: '\u001b[32msuccess message\u001b[m'
+        },
+        {
+            value: warn('warn message'),
+            result: undefined
+        },
+        {
+            value: warn('warn message', true),
+            result: '\u001b[33mwarn message\u001b[m'
+        },
+        {
+            value: error('error message'),
+            result: undefined
+        },
+        {
+            value: error('error message', true),
+            result: '\u001b[31merror message\u001b[m'
+        }
+    ];
+
+    toEqualData.forEach((element) => {
+        expect(element.value).toEqual(element.result);
+    });
 });
