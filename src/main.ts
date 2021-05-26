7920585c0e8cbf8f16334d39fb2f3177ea17fd08
Slime-hatena <git@aki-memo.net>
2021-05-27 01:32:40 +0900

    feat: add the main code of the action

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,48 @@
 import * as core from '@actions/core';
-import {wait} from './wait';
+import * as fs from 'fs';
+import {Changelog} from './Changelog';
+import {info, success, error, br} from '../src/Output';
 
 async function run(): Promise<void> {
     try {
-        const ms: string = core.getInput('milliseconds');
-        core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+        process.env['GITHUB_AUTH'] = core.getInput('GITHUB_AUTH');
+        const LABEL_SETTING_FILE_PATH = core.getInput(
+            'LABEL_SETTING_FILE_PATH'
+        );
+        const TAG_FROM = core.getInput('TAG_FROM');
+        const TAG_TO = core.getInput('TAG_TO');
+        const REMOVE_TITLE_LINE =
+            core.getInput('REMOVE_TITLE_LINE').toLowerCase() === 'true';
 
-        core.debug(new Date().toTimeString());
-        await wait(parseInt(ms, 10));
-        core.debug(new Date().toTimeString());
+        const labelSettings = JSON.parse(
+            fs.readFileSync(LABEL_SETTING_FILE_PATH, 'utf8')
+        );
+        const changelog = new Changelog(labelSettings);
+        changelog.repo = process.env.GITHUB_REPOSITORY ?? '';
+        let markdown = await changelog.generate(TAG_FROM, TAG_TO);
 
-        core.setOutput('time', new Date().toTimeString());
-    } catch (error) {
-        core.setFailed(error.message);
+        if (REMOVE_TITLE_LINE) {
+            markdown = markdown.substr(
+                markdown.indexOf(
+                    '\n',
+                    markdown.indexOf('\n', markdown.indexOf('\n', 0) + 1) + 1
+                ) + 1
+            );
+        }
+
+        if (markdown.length === 0) {
+            error(
+                'The changelog was not generated. Please check the label settings and whether the pull requests have been merged.'
+            );
+        } else {
+            success('Changelog has been generated.');
+            info(markdown);
+        }
+        br();
+
+        core.setOutput('markdown', markdown);
+    } catch (e) {
+        core.setFailed(e.message);
     }
 }
 
